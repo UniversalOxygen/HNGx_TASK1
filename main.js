3c39b157701a8a3faa57ef43aac2175122521bf5
unknown <olagoketunde600@gmail.com>
2023-09-10 09:10:33 -0400

    background animation script

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
 const background = document.querySelector('#background');
 const hoverElements = document.querySelectorAll('#hover-el');
 const container = document.querySelector('#container');
+
+gsap.to('#background', {
+    y: '-50vh',
+    duration: 1, // Animation duration (in seconds)
+    backgroundColor: '#022842', // Intermediate color 1
+    ease: 'back(2)',
+    onComplete: function () {
+      // Animation is complete for Intermediate color 1
+      gsap.to('#background', {
+        y: '50vh',
+        duration: 1, // Animation duration for Intermediate color 2
+        backgroundColor: '#805f06', // Intermediate color 2
+        ease: 'back(2)',
+        onComplete: function () {
+          // Animation is complete for Intermediate color 2
+          gsap.to('#background', {
+            y: '0vh',
+            duration: 1, // Animation duration for the final color
+            backgroundColor: '#6d140a', // Final background color
+            ease: 'back(2)',
+            onComplete: function () {
+              const tl = gsap.timeline({
+                defaults: { opacity: 0, ease: 'back' },
+              });
+              gsap.set('#container', {
+                visibility: 'visible',
+                delay: '3.2',
+                ease: 'back',
+              });
+  
+              tl.from('#container', { ease: 'linear', autoAlpha: 0 })
+                .from('h1', { x: 80 })
+                .from('h3', { x: -80 })
+                .from('a', { y: -30 })
+                .from('.time', { y: 30 });
+            },
+          });
+        },
+      });
+    },
+  });
